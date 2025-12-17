b64f99719de52fdeebe8a1e8afac6bb636324d8a
Matushkin Roman <rmatushkin@icloud.com>
2025-12-17 21:09:07 +0300

    init commit

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0', '-V, --version', 'output the version number');
+
+
+program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export default (filepath1, filepath2) => {
+  // Пока возвращаем заглушку
+  return `Comparison between ${filepath1} and ${filepath2}`;
+};
